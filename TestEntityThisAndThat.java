e8bc3b27e9dcb0a18dbed3d72e822fc9c0468066
abkazan <abkazan@wisc.edu>
2023-04-14 17:45:52 -0500

    testConstructor

--- TestEntityThisAndThat.java
+++ TestEntityThisAndThat.java
@@ -1,7 +1,14 @@
 import devtech3.EntityThis;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 class TestEntityThis {
+    
+    @Test
+    public void testConstructor() {
+        this = new EntityThis();
+        assertNotEquals(null, this)
+    }
 
     
 }
