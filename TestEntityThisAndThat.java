36db04f80d31274dc3a1dd117039dd3f1bcfd6ef
jdhopwood <jdhopwood@wisc.edu>
2023-04-14 17:34:19 -0500

    junit

--- TestEntityThisAndThat.java
+++ TestEntityThisAndThat.java
@@ -1,5 +1,17 @@
+import junit.framework.*;
 
-class TestEntityThisAndThat {
+public class TestEntityThisAndThat extends TestCase {
+   protected int value1, value2;
+   
+   // assigning the values
+   protected void setUp(){
+      value1 = 3;
+      value2 = 3;
+   }
 
-    
+   // test method to add two values
+   public void testAdd(){
+      double result = value1 + value2;
+      assertTrue(result == 6);
+   }
 }
